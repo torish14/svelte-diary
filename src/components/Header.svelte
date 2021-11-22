3e1c80ccc810e9be5289e532bb936cba75ba050d
torish14 <nanamin.104@icloud.com>
2021-11-22 15:23:57 +0900

    ✨ feat: ページのヘッダーにルーティングを表示

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,1 +1,12 @@
-<h1>Header</h1>
+<script>
+import { Router, Link, Route } from 'svelte-routing'
+</script>
+
+
+<header>
+	<Router>
+    <Link to="/">Home</Link>
+    <Link to="about">About</Link>
+
+  </Router>
+</header>
