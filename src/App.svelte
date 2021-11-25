0478a7f261796c0f1eb1ada049642dc3b65ce038
torish14 <nanamin.104@icloud.com>
2021-11-25 13:44:17 +0900

    👗 style: フォーマット の修正

--- src/App.svelte
+++ src/App.svelte
@@ -1,28 +1,39 @@
 <script>
-import "smelte/src/tailwind.css"
-import { Router, Link, Route } from 'svelte-routing'
-import Header from './components/Header.svelte'
-import Footer from './components/Footer.svelte'
-import Home from './components/Home.svelte'
+import 'smelte/src/tailwind.css'
+import { Route, Router } from 'svelte-routing'
+import About from './components/About.svelte'
 import Create from './components/Create.svelte'
 import Diary from './components/Diary.svelte'
-import About from './components/About.svelte'
+import Footer from './components/Footer.svelte'
+import Header from './components/Header.svelte'
+import Home from './components/Home.svelte'
 
 export let url
 </script>
 
 <main class="bg-background-500">
-	<Header />
+  <Header />
 
-	<Router url="{url}">
-		<Route path="home" component="{Home}" />
-		<Route path="diary/:id" component="{Diary}" />
-		<Route path="create" component="{Create}" />
-		<Route path="about" component="{About}" />
-	</Router>
+  <section class="content">
+    <Router url={url}>
+      <Route path="/" component={Home} />
+      <Route path="diary/:id" component={Diary} />
+      <Route path="create" component={Create} />
+      <Route path="about" component={About} />
+    </Router>
+  </section>
 
   <Footer />
 </main>
 
 <style>
+main {
+  text-align: center;
+  max-width: 640px;
+  margin: 0 auto;
+}
+
+.content {
+  min-height: calc(100vh - 60px - 60px);
+}
 </style>
