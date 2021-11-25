12e5441b3a1bad26e621d8f6c0f20e350acfb518
torish14 <nanamin.104@icloud.com>
2021-11-25 15:32:33 +0900

    ✨ feat: ヘッダーにハンバーガーメニューとアイコンを表示

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,13 +1,26 @@
+<svelte:head>
+	<!-- Import base css -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/svelte-hamburgers@3/dist/css/base.css" />
+	<!-- Import spin css (spin is default type) -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/svelte-hamburgers@3/dist/css/types/spin.css" />
+</svelte:head>
+
 <script>
 import { Router, Link } from 'svelte-routing'
+import Hamburger from 'svelte-hamburgers'
+import BrightnessContrast32 from 'carbon-icons-svelte/lib/BrightnessContrast32/BrightnessContrast32.svelte'
+
+let open
 </script>
 
-<header class="bg-primary-900">
-  <Router>
+<header class="bg-primary-900 flex justify-between items-center">
+  <Hamburger bind:open --color="white" />
+  <!-- <Router>
     <Link to="/">Home</Link>
     <Link to="about">About</Link>
     <Link to="create">Create</Link>
-  </Router>
+  </Router> -->
+  <BrightnessContrast32 class="mr-6 text-white-900" />
 </header>
 
 <style>
