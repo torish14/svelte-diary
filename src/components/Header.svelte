df41609050bb42d38f6a2e7c732be58fa702ee1a
torish14 <nanamin.104@icloud.com>
2021-11-25 15:48:09 +0900

    👗 style: フォーマットの修正

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,10 +1,3 @@
-<svelte:head>
-	<!-- Import base css -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/svelte-hamburgers@3/dist/css/base.css" />
-	<!-- Import spin css (spin is default type) -->
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/svelte-hamburgers@3/dist/css/types/spin.css" />
-</svelte:head>
-
 <script>
 import { Router, Link } from 'svelte-routing'
 import Hamburger from 'svelte-hamburgers'
@@ -12,6 +5,19 @@ import BrightnessContrast32 from 'carbon-icons-svelte/lib/BrightnessContrast32/B
 
 let open
 </script>
+
+<svelte:head>
+  <!-- Import base css -->
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/svelte-hamburgers@3/dist/css/base.css"
+  />
+  <!-- Import spin css (spin is default type) -->
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/svelte-hamburgers@3/dist/css/types/spin.css"
+  />
+</svelte:head>
 
 <header class="bg-primary-900 flex justify-between items-center">
   <Hamburger bind:open --color="white" />
