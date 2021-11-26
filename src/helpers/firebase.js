0502730290dc5326bb05534cc063909d38b3c056
torish14 <nanamin.104@icloud.com>
2021-11-26 21:25:42 +0900

    ⛏ chore: firebase のインストール

---
+++ src/helpers/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAyU7zFc0Mb5t0h0PeQQaqlU18YKhSHe8c',
+  authDomain: 'svelte-diary-476e2.firebaseapp.com',
+  projectId: 'svelte-diary-476e2',
+  storageBucket: 'svelte-diary-476e2.appspot.com',
+  messagingSenderId: '746732041807',
+  appId: '1:746732041807:web:61911b27617c04634e1c3a',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
