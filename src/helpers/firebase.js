b33c0b54bf3a118fb247ef96c81daccbac37ab94
torish14 <nanamin.104@icloud.com>
2021-11-27 18:08:11 +0900

    ✨ feat: firebase の設定を記述

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -5,13 +5,14 @@ import { initializeApp } from 'firebase/app'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAyU7zFc0Mb5t0h0PeQQaqlU18YKhSHe8c',
-  authDomain: 'svelte-diary-476e2.firebaseapp.com',
-  projectId: 'svelte-diary-476e2',
-  storageBucket: 'svelte-diary-476e2.appspot.com',
-  messagingSenderId: '746732041807',
-  appId: '1:746732041807:web:61911b27617c04634e1c3a',
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
+console.log(app)
