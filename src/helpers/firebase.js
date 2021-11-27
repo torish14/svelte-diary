3b5bc404af75494413a9f3997db911cb33548409
torish14 <nanamin.104@icloud.com>
2021-11-27 19:50:36 +0900

    ✨ feat: userId を store.js からインポート

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,24 +1,35 @@
 // Import the functions you need from the SDKs you need
+// eslint-disable-next-line
 import { initializeApp } from 'firebase/app'
 import {
+  // eslint-disable-next-line
   getAuth,
+  // eslint-disable-next-line
   signInWithPopup,
+  // eslint-disable-next-line
   GoogleAuthProvider,
 } from 'firebase/auth'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { userId } from '../store'
+import Cookies from 'js-cookie'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  // eslint-disable-next-line
   apiKey: FIREBASE_API_KEY,
+  // eslint-disable-next-line
   authDomain: FIREBASE_AUTH_DOMAIN,
+  // eslint-disable-next-line
   projectId: FIREBASE_PROJECT_ID,
+  // eslint-disable-next-line
   storageBucket: FIREBASE_STORAGE_BUCKET,
+  // eslint-disable-next-line
   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  // eslint-disable-next-line
   appId: FIREBASE_APP_ID,
 }
 
 // Initialize Firebase
+// eslint-disable-next-line
 const app = initializeApp(firebaseConfig)
 
 const provider = new GoogleAuthProvider()
@@ -30,19 +41,26 @@ export const signInWithGoogle = () => {
       // This gives you a Google Access Token. You can use it to access the Google API.
       const credential =
         GoogleAuthProvider.credentialFromResult(result)
+      // eslint-disable-next-line
       const token = credential.accessToken
       // The signed-in user info.
       const user = result.user
+      userId.set(result.user.uid)
       console.log(user)
+      Cookies.set('uid', result.user.uid)
       // ...
     })
     .catch((error) => {
       // Handle Errors here.
+      // eslint-disable-next-line
       const errorCode = error.code
+      // eslint-disable-next-line
       const errorMessage = error.message
       // The email of the user's account used.
+      // eslint-disable-next-line
       const email = error.email
       // The AuthCredential type that was used.
+      // eslint-disable-next-line
       const credential = GoogleAuthProvider.credentialFromError(error)
       // ...
     })
