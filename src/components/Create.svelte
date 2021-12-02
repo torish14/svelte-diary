591d1fad12686f50aa9be15775b52fc25e418214
torish14 <nanamin.104@icloud.com>
2021-12-02 15:52:25 +0900

    ✨ feat: submit関数の追加

--- src/components/Create.svelte
+++ src/components/Create.svelte
@@ -3,12 +3,16 @@ import { Slider, TextField, Button } from 'smelte'
 
 let rate = 5
 let body = ''
+
+const submit = () => {
+  // firestore へ POSTする関数を呼び出す
+}
 </script>
 
 <h3>日記を書こう！</h3>
-<form class="p-5">
+<form class="p-5" on:submit={submit}>
   <p class="mb-4">今日の気分は{rate}点です</p>
   <Slider class="mb-4" min="0" max="10" bind:value={rate} />
   <TextField label="日記の本文" class="bg-white-900" bind:value={body} textarea rows="5" outlined />
-  <Button class="text-white-900">日記を保存</Button>
+  <Button type="submit" class="text-white-900">日記を保存</Button>
 </form>
