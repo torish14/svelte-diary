ce87fff8c72bb0685cd0a9eb7414110740d511b0
torish14 <nanamin.104@icloud.com>
2021-12-03 02:32:46 +0900

    🐞 fix: await があるため、async を追記

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -5,13 +5,17 @@ import dayjs from 'dayjs'
 
 // Add a new document with a generated id.
 export const postDiary = (uid = '', rate = 1, body = '') => {
-  console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'))
-  const docRef = addDoc(collection(db, 'diaries'), {
-    uid: uid,
-    rate: rate,
-    body: body,
-    image: '',
-    createdAt: dayjs().format('YYYY-MM-DD HH:mm:ss'),
-  })
-  console.log('Document written with ID: ', docRef.id)
+  async function setFirestore() {
+    const docRef = await addDoc(collection(db, 'diaries'), {
+      uid: uid,
+      rate: rate,
+      body: body,
+      image: '',
+      createdAt: dayjs().format('YYYY/MM/DD HH:mm:ss'),
+    })
+    console.log('Document written with ID: ', docRef.id)
+    // もしデータの追加に成功したら、true を、失敗なら false を返す
+    // return docRef.id ? true : false
+  }
+  setFirestore()
 }
