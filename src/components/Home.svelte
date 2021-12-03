74e6690ee9f53cfbdaf735c957cbfc4b489bf189
torish14 <nanamin.104@icloud.com>
2021-12-04 04:46:56 +0900

    ⛏ chore: svelte-routing と svelte-star-rating の読み込み

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,9 +1,11 @@
 <script>
 // ライフサイクルについてのドキュメント：https://tech-wiki.online/jp/svelte-lifecycle-events.html
 import { onMount, onDestroy } from 'svelte'
+import { Router, Link } from 'svelte-routing'
 import { Button } from 'smelte'
 import { userId } from '../store'
 import { fetch } from '../helpers/api'
+import StarRating from 'svelte-star-rating'
 
 let uid
 const unsubscribe = userId.subscribe(id => uid = id)
