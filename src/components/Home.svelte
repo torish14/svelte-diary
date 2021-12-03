7dcbbfa316c4f9af2a421d890513a11a2c3b1324
torish14 <nanamin.104@icloud.com>
2021-12-04 01:45:12 +0900

    ✨ feat: api.js から fetch関数（firestore のコレクションからドキュメントを取得する）を読み込んで実行

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -1,5 +1,18 @@
 <script>
+// ライフサイクルについてのドキュメント：https://tech-wiki.online/jp/svelte-lifecycle-events.html
+import { onMount, onDestroy } from 'svelte'
 import { Button } from 'smelte'
+import { userId } from '../store'
+import { fetch } from '../helpers/api'
+
+let uid
+const unsubscribe = userId.subscribe(id => uid = id)
+let promise
+onMount(async () => {
+  promise = await fetch(uid)
+  console.log(promise)
+})
+onDestroy(() => { unsubscribe })
 </script>
 
 <h1>Home</h1>
