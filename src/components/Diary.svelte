b37ef7024dd5dce94296a93e78f6f1ca4aee83d5
torish14 <nanamin.104@icloud.com>
2021-12-06 10:25:40 +0900

    ✨ feat: 日記の詳細ページの作成

--- src/components/Diary.svelte
+++ src/components/Diary.svelte
@@ -1,1 +1,12 @@
-<h1>Diary</h1>
+<script>
+import { onMount } from 'svelte'
+import { getDiary } from '../helpers/api'
+export let id
+console.log(id)
+
+let diary
+onMount(async () => {
+  diary = await getDiary(id)
+  console.log(diary)
+})
+</script>
