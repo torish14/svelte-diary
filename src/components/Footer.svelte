319a2ee86ef61cf1e698e8fa562796b9ffa3b888
torish14 <nanamin.104@icloud.com>
2022-01-03 16:06:15 +0900

    ✨ feat: フッターにリンクを追加

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,4 +1,27 @@
-<footer class="bg-secondary-900">Footer</footer>
+<script>
+import { onDestroy } from 'svelte';
+import { Router, Link } from 'svelte-routing';
+import { userId } from '../store'
+let uid
+
+const unsbscribe = userId.subscribe((id) => {
+  uid = id
+})
+
+onDestroy(() => {
+  unsbscribe
+})
+</script>
+
+<footer class="bg-secondary-900 flex items-center justify-around">
+  <Router>
+    <Link class="block mb-3" to="/">Home</Link>
+    <Link class="block mb-3" to="about">日記の効果とは？</Link>
+    {#if uid}
+      <Link class="block mb-3" to="create">日記を書く</Link>
+    {/if}
+  </Router>
+</footer>
 
 <style>
 footer {
