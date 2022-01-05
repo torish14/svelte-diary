605b1eb408a6efe398f099e06dcde1b0ec4249df
torish14 <nanamin.104@icloud.com>
2022-01-05 15:18:01 +0900

    👗 style: ダークモードで bg-dark-900

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -13,7 +13,7 @@ onDestroy(() => {
 })
 </script>
 
-<footer class="bg-secondary-900 flex items-center justify-around">
+<footer class="bg-secondary-900 dark:bg-dark-900 flex items-center justify-around">
   <Router>
     <Link class="block mb-3" to="/">Home</Link>
     <Link class="block mb-3" to="about">日記の効果とは？</Link>
